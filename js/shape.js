58042e88765ae7cbb19357e3b2f79b2abb263ac1
Keith Potempa <keithrpotempa@gmail.com>
2020-04-14 16:01:23 -0500

    fixed shape issues, tweaked headers

--- js/shape.js
+++ js/shape.js
@@ -1,36 +1,11 @@
-$(function(){
-	
-	var
-    $outerSquare = $('.cube'),
-		$menus		 = $('a.item'),
-		$innerSquare = $('.center'),
-		$menuParent	= $menus.parent(),
-		$box 			 = $('.shape')
-    ;
-	
-	$outerSquare
-   .click('$flip', function() {
-		$box.shape('flip up');
-		})
-  ;
-
-  $menus.detach();
-    $menus
-      .click( '$menus', function() {
-          $(this)
-          .addClass('active')
-            .siblings('.item')
-              .removeClass('active');
-    
-      })
-  ;
-
-  $menuParent.append( $menus );
-    
-    $menus.on('click', function(attr) {
-      var tabId = $(this).attr('data-tab');
-      $box.shape(tabId);
-    })
-  ;
-	
+// Flip buttons
+$(".ui.button.left").click(function( event ) {
+  event.preventDefault();
+  $('.shape').shape('flip left');
 });
+$(".ui.button.right").click(function( event ) {
+  event.preventDefault();
+  $('.shape').shape('flip right');
+});
+
+$('.segment').dimmer('show');
